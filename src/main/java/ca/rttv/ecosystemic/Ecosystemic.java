82a8295b659da36ed6de23acea10b3ba63934480
RTTV <tristanandlucky@gmail.com>
2022-05-24 20:52:41 -0400

    <init>

---
+++ src/main/java/ca/rttv/ecosystemic/Ecosystemic.java
@@ -0,0 +1,11 @@
+package ca.rttv.ecosystemic;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+
+public class Ecosystemic implements ModInitializer {
+	@Override
+	public void onInitialize(ModContainer mod) {
+
+	}
+}
